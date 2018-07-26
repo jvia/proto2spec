1b68e98772712ec5f7d6ef765bb63a58c80d1453
Jeremiah Via <jeremiah.via@nytimes.com>
2018-07-26 13:31:52 -0600

    Version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.gamayun/proto2spec "0.2.0-SNAPSHOT"
+(defproject io.gamayun/proto2spec "0.2.0"
   :description "A Clojure Spec generator for Google Protocol Buffers."
   :url "https://github.com/jvia/proto2spec"
   :license {:name "Eclipse Public License"
